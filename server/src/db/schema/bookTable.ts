a8a02a4f2ef1688015034da39b2bd755ce37adc6
Предраг Николић <idmpepe@gmail.com>
2024-11-05 19:14:08 +0100

    don't allow deleting books, instead introduce status to mark a book "Hidden" or "Available"

--- server/src/db/schema/bookTable.ts
+++ server/src/db/schema/bookTable.ts
@@ -1,6 +1,9 @@
 import { pgTable, uuid, timestamp,text, integer } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod"
+import { z } from "zod";
 
+const BOOK_STATUSES = ['available', 'hidden'] as const
+type BookStatus = typeof BOOK_STATUSES[number]
 
 export const bookTable = pgTable("book", {
     id: uuid().primaryKey().defaultRandom(),
@@ -11,9 +14,12 @@ export const bookTable = pgTable("book", {
     title: text().notNull(),
     price: integer().notNull(),
     description: text().notNull(),
+    status: text().notNull().$type<BookStatus>().default('available'),
 });
 
-export const insertBookSchema = createInsertSchema(bookTable).omit({
+export const insertBookSchema = createInsertSchema(bookTable, {
+    status: z.enum(BOOK_STATUSES)
+}).omit({
     id: true
 })
 export const bookSchema = createSelectSchema(bookTable)
